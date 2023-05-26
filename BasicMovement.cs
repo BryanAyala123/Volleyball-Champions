5fe5a6bff083c41d78e9cf797961c66f0d6d559f
BryanAyala123 <94377412+BryanAyala123@users.noreply.github.com>
2023-05-26 08:52:33 -0400

    Update BasicMovement.cs

--- BasicMovement.cs
+++ BasicMovement.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BacisMovement : MonoBehaviour{
+public class BasicMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float moveSpeed1 = 10f;
+    public Rigidbody2D p1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1 = GetComponent<Rigidbody2D>();
 
-    void Start(){
-        
     }
 
-    void Update(){
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"),0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime*moveSpeed;
+        if (Input.GetKeyDown(KeyCode.W)){
+            p1.velocity = new Vector2(p1.velocity.x,moveSpeed1);
 
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
-
+        }
     }
 }
